ba3e73c3d8aa9434e4fef81fed2d4135faaf6e52
Dave Bernhard <davidbernhard@live.com>
2022-04-10 09:49:53 +0100

    make images responsive and fix aspect ratio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,41 +2,45 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const Hero = () => {
-  return (
-    <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <picture>
+                <source srcSet="/images/hero-img.avif 1x, /images/hero-img.avif@2x 2x, /images/hero-img.avif@3x 3x" />
+                <source srcSet="/images/hero-img.jpg 1x, /images/hero-img.jpg@2x 2x, /images/hero-img.jpg@3x 3x" />
+                <HeroImage alt="A beautiful cat" src="/images/hero-img.jpg" />
+            </picture>
+            <Swoop src="/swoop.svg" />
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.section`
-  position: relative;
-  height: 60vh;
-  min-height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  background: hsl(0deg 0% 1%);
+    position: relative;
+    height: 60vh;
+    min-height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    background: hsl(0deg 0% 1%);
 `;
 
 const HeroImage = styled.img`
-  display: block;
-  width: 500px;
-  height: 500px;
-  max-height: 100%;
+    display: block;
+    width: 500px;
+    height: 500px;
+    max-height: 100%;
 `;
 
 const Swoop = styled.img`
-  position: absolute;
-  left: 0;
-  right: 0;
-  /*
+    position: absolute;
+    left: 0;
+    right: 0;
+    /*
     Overhang by a couple px to prevent any pixel-rounding
     display issues
   */
-  bottom: -2px;
-  width: 100%;
+    bottom: -2px;
+    width: 100%;
 `;
 
 export default Hero;
